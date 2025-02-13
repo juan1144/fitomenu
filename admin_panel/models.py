73cbe467e36221a3450151590a0037cd321765d1
Juan Hernández <jfph25@gmail.com>
2025-02-12 21:10:53 -0600

    Add admin_panel and start model makemigrations

---
+++ admin_panel/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=255)
+    descripcion = models.TextField(blank=True, null=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    imagen = models.ImageField(upload_to="productos/")
+    disponible = models.BooleanField(default=True)
+    categoria = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
