8885ed849886aaf2f57c1bef91af27fa85383e5c
Juan Hernández <jfph25@gmail.com>
2025-03-11 15:48:53 -0600

    Adding a new path for media files
    Added a different configuration for media files. Now it will store on media/ directory ensuring the best practicing and a clean structure of all the proyect.

--- admin_panel/models.py
+++ admin_panel/models.py
@@ -5,7 +5,7 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=255)
     descripcion = models.TextField(blank=True, null=True)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
-    imagen = models.ImageField(upload_to="productos/")
+    imagen = models.ImageField(upload_to="productos/", blank=True, null=True)
     disponible = models.BooleanField(default=True)
     categoria = models.CharField(max_length=100)
 
