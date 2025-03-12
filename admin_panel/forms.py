2056a5b5546669d955fedd65f28ee0387add45de
Juan Hernández <jfph25@gmail.com>
2025-03-12 11:39:30 -0600

    Added new table for category and removed descripcion column
    Added a new table to manage categories and ensure consistence in app. Modify producto model to remove descripcion column and update forms and template accordigly.

--- admin_panel/forms.py
+++ admin_panel/forms.py
@@ -4,4 +4,4 @@ from .models import Producto
 class ProductoForm(forms.ModelForm):
     class Meta:
         model = Producto
-        fields = ["nombre", "descripcion", "precio", "imagen", "disponible", "categoria"]
+        fields = ["nombre", "precio", "imagen", "disponible", "categoria"]
--- admin_panel/models.py
+++ admin_panel/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 
+class CategoriaProducto(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.nombre
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=255)
-    descripcion = models.TextField(blank=True, null=True)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     imagen = models.ImageField(upload_to="productos/", blank=True, null=True)
     disponible = models.BooleanField(default=True)
-    categoria = models.CharField(max_length=100)
+    categoria = models.ForeignKey(CategoriaProducto, on_delete=models.CASCADE, related_name="productos")
 
     def __str__(self):
         return self.nombre
