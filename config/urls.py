76673411fc0f50b03424f5ab79be14c681e41cd3
Juan Hernández <jfph25@gmail.com>
2025-04-10 15:22:39 -0600

    Add menu as main url

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("admin_panel/", include("admin_panel.urls", namespace='admin_panel')),
-    path("menu/", include("menu.urls", namespace='menu')),
+    path("", include("menu.urls", namespace='menu')),
     path("kitchen/", include("kitchen_panel.urls", namespace='kitchen')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
