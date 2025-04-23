27423b06327346215b01b1b725959b8db958f113
Juan Hernández <jfph25@gmail.com>
2025-04-23 14:36:31 -0600

    Comment django admin panel url

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,10 @@
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    # path("admin/", admin.site.urls),
     path("admin_panel/", include("admin_panel.urls", namespace="admin_panel")),
     path("", include("menu.urls", namespace="menu")),
     path("kitchen/", include("kitchen_panel.urls", namespace="kitchen")),
