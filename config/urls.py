bdd02383d659e427a188d172dcc105c2d6519d14
Juan Hernández <jfph25@gmail.com>
2025-04-29 15:07:57 -0600

    Fixing views import in urls from config

--- config/urls.py
+++ config/urls.py
@@ -1,17 +1,17 @@
 # from django.contrib import admin
-import core.views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     # path("admin/", admin.site.urls),
     path(
         "login/",
-        core.views.LoginView.as_view(template_name="login/login.html"),
+        LoginView.as_view(template_name="login/login.html"),
         name="login",
     ),
-    path("logout/", core.views.LogoutView.as_view(), name="logout"),
+    path("logout/", LogoutView.as_view(), name="logout"),
     path("admin_panel/", include("admin_panel.urls", namespace="admin_panel")),
     path("", include("menu.urls", namespace="menu")),
     path("kitchen/", include("kitchen_panel.urls", namespace="kitchen")),
